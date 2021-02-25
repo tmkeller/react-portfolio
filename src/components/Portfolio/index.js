ffe7dbc20772eb15eba34a1710a762fb59ba4ba8
Tim Keller <timothy.m.keller@gmail.com>
2021-02-25 13:22:19 -0800

    Added portfolio for google-books-search

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -11,6 +11,7 @@ import Quiz from "./images/online-quiz.jpg";
 import TurnTables from "./images/turn-the-tables.jpg";
 import Weather from "./images/weather-dashboard.jpg";
 import Scheduler from "./images/workday-scheduler.jpg";
+import Books from "./images/google-books-search.jpg";
 
 function Portfolio() {
     return (
@@ -21,69 +22,77 @@ function Portfolio() {
                 </section>
                 <hr/>
                 <section className="row custom_row">
-                    <PortfolioItem 
-                        title="Weather Dashboard" 
-                        githubURL="https://github.com/tmkeller/weather-dashboard" 
-                        deployed="https://tmkeller.github.io/weather-dashboard/"
-                        image={ Weather }
-                    />
-                    <PortfolioItem 
-                        title="Turn the Tables" 
-                        githubURL="https://github.com/tmkeller/eightysixed-app" 
-                        deployed="https://peaceful-gorge-36427.herokuapp.com/"
-                        image={ TurnTables }
-                    />
+                <PortfolioItem 
+                    title="Google Books Search" 
+                    githubURL="https://github.com/tmkeller/google-books-search" 
+                    deployed="https://tmk-google-books-search.herokuapp.com/"
+                    image={ Books }
+                />
+                <PortfolioItem 
+                    title="Weather Dashboard" 
+                    githubURL="https://github.com/tmkeller/weather-dashboard" 
+                    deployed="https://tmkeller.github.io/weather-dashboard/"
+                    image={ Weather }
+                />
                 </section>
                 <section className="row custom_row">
-                    <PortfolioItem 
-                        title="Online Quiz" 
-                        githubURL="https://github.com/tmkeller/online-quiz" 
-                        deployed="https://tmkeller.github.io/online-quiz/"
-                        image={ Quiz }
-                    />
-                    <PortfolioItem 
-                        title="Workday Scheduler" 
-                        githubURL="https://github.com/tmkeller/workday-scheduler" 
-                        deployed="https://tmkeller.github.io/workday-scheduler/"
-                        image={ Scheduler }
-                    />
+                <PortfolioItem 
+                    title="Turn the Tables" 
+                    githubURL="https://github.com/tmkeller/eightysixed-app" 
+                    deployed="https://peaceful-gorge-36427.herokuapp.com/"
+                    image={ TurnTables }
+                />
+                <PortfolioItem 
+                    title="Online Quiz" 
+                    githubURL="https://github.com/tmkeller/online-quiz" 
+                    deployed="https://tmkeller.github.io/online-quiz/"
+                    image={ Quiz }
+                />
                 </section>
                 <section className="row custom_row">
-                    <PortfolioItem 
-                        title="NEBHE"
-                        deployed="https://nebhe.org/"
-                        image={ NEBHE }
-                    />
-                    <PortfolioItem 
-                        title="American Vegetable Oil"
-                        deployed="https://www.americanvegoil.com/"
-                        image={ Avo }
-                    />
+                <PortfolioItem 
+                    title="Workday Scheduler" 
+                    githubURL="https://github.com/tmkeller/workday-scheduler" 
+                    deployed="https://tmkeller.github.io/workday-scheduler/"
+                    image={ Scheduler }
+                />
+                <PortfolioItem 
+                    title="NEBHE"
+                    deployed="https://nebhe.org/"
+                    image={ NEBHE }
+                />
                 </section>
                 <section className="row custom_row">
-                    <PortfolioItem 
-                        title="CSU Chico"
-                        deployed="https://today.csuchico.edu/"
-                        image={ Chico }
-                    />
-                    <PortfolioItem 
-                        title="John Livesay"
-                        deployed="https://johnlivesay.com/"
-                        image={ Livesay }
-                    />
+                <PortfolioItem 
+                    title="American Vegetable Oil"
+                    deployed="https://www.americanvegoil.com/"
+                    image={ Avo }
+                />
+                <PortfolioItem 
+                    title="CSU Chico"
+                    deployed="https://today.csuchico.edu/"
+                    image={ Chico }
+                />
                 </section>
                 <section className="row custom_row">
-                    <PortfolioItem 
-                        title="My Rehab Exercise"
-                        deployed="https://myrehabexercise.com/"
-                        image={ MRE }
-                    />
-                    <PortfolioItem 
-                        title="Burger Logger"
-                        deployed="https://handlebars-burger-logger.herokuapp.com/"
-                        image={ Burger }
-                        githubURL={ "https://github.com/tmkeller/Handlebars-Burger-Logger" }
-                    />
+                <PortfolioItem 
+                    title="John Livesay"
+                    deployed="https://johnlivesay.com/"
+                    image={ Livesay }
+                />
+                <PortfolioItem 
+                    title="My Rehab Exercise"
+                    deployed="https://myrehabexercise.com/"
+                    image={ MRE }
+                />
+                </section>
+                <section className="row custom_row">
+                <PortfolioItem 
+                    title="Burger Logger"
+                    deployed="https://handlebars-burger-logger.herokuapp.com/"
+                    image={ Burger }
+                    githubURL={ "https://github.com/tmkeller/Handlebars-Burger-Logger" }
+                />
                 </section>
             </article>
         </section>
